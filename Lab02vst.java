5b4cdc7b51e333ad4c1b12e0986272648a4dfed9
zach <zachary.schwerdtfeger@vtsd.com>
2022-09-15 12:29:19 -0400

    done

--- Lab02vst.java
+++ Lab02vst.java
@@ -1,7 +1,14 @@
-public class Lab02vst {
-
-    public static void main(String args[]){
-        // Example changes
+public class Lab02vst
+{
+    public static void main (String[] args) {
+        System.out.println("Lab 02, 80 point version ");
+        System.out.println();
+        System.out.println();
+        System.out.println("measure today's knowledge by yesterday's confusion ");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("");
 
     }
 
